d23a3bacd15d8c382f11dd5ef5cf2cf5b5f3f0cd
Kenan Babic <kenanbc223@gmail.com>
2026-01-03 13:30:03 +0100

    Added new routes for edit profile and modified avatar showing, styles

--- ipia-p2/src/app/view-my-profile/view-my-profile.ts
+++ ipia-p2/src/app/view-my-profile/view-my-profile.ts
@@ -1,10 +1,10 @@
-import { Component, inject } from '@angular/core';
+ import { Component, inject } from '@angular/core';
 import { AuthService } from '../auth';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-view-my-profile',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './view-my-profile.html',
   styleUrl: './view-my-profile.css',
 })
@@ -18,4 +18,39 @@ export class ViewMyProfile {
        this.router.navigate(['/login']);
      });
    }
+
+   formatUserProfileStudyProgram() {
+      const profile = this.auth.userProfile();
+      if (!profile) return 'Nema dostupnog profila.';
+      console.log(profile);
+      switch (profile.studyProgram) {
+         case "1":
+            return 'Informatika i računarstvo';
+         case '2':
+            return 'Informacione tehnologije';
+          case '3':
+            return 'Tržišne komunikacije';
+          case '4':
+            return 'Savremeno poslovanje';
+         default:
+            return 'Nepoznat smjer';
+      }
+   }
+
+   formatUserProfileYearOfStudy() {
+      const profile = this.auth.userProfile();
+      if (!profile) return 'Nema dostupnog profila.';
+      switch (profile.yearOfStudy) {
+         case '1':
+            return 'I godina';
+          case '2':
+            return 'II godina';
+          case '3':
+            return 'III godina';
+          case '4':
+            return 'IV godina';
+          default: 
+            return 'Nepoznata godina studija';
+      }
+    }
 }
